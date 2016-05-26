f1e225a17187ad621e998e212ac59f7b42824ec4
Thorn Walli <thorn.walli@grabarzundpartner.intra>
2016-05-26 22:53:01 +0200

    utils/merge fix

--- src/js/utils/merge.js
+++ src/js/utils/merge.js
@@ -1,10 +1,10 @@
 "use strict";
 
-var uniq = require('lodash/uniqBy');
-var union = require('lodash/union');
+var uniq = require('lodash/uniq');
+var unionBy = require('lodash/unionBy');
 
 module.exports = {
     collections: function(collectionA, collectionB, by) {
-        return uniq(union(collectionB, collectionA), by);
+        return uniq(unionBy(collectionB, collectionA, by), false, by);
     }
 };
