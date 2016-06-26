0cf6f3a69c5328dadbf4a8cdb1cab28cb53bf222
Stephan Gerbeth <stephan.gerbeth@gmail.com>
2016-06-26 20:00:13 +0200

    repositioned statsjs

--- src/js/embed/performance/stats.js
+++ src/js/embed/performance/stats.js
@@ -21,10 +21,12 @@ function isStatsEnabled() {
 function loadStatsScript() {
     script.load('//rawgit.com/mrdoob/stats.js/master/build/stats.min.js', function() {
         var stats = new global.Stats();
+        stats.dom.style.left = 'auto';
+        stats.dom.style.right = '0';
         document.body.appendChild(stats.dom);
         requestAnimationFrame(function loop(){
             stats.update();
             global.requestAnimationFrame(loop);
         });
-    });    
+    });
 }
