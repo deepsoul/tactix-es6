38b2c5d0208df3d1034bf9f8d140ab30c15487f9
Thorn Walli <thorn.walli@grabarzundpartner.de>
2016-09-24 16:44:33 +0200

    font classname replace spaces with "-"

--- src/js/embed/fontfaceObserver.js
+++ src/js/embed/fontfaceObserver.js
@@ -6,7 +6,7 @@ var modernizr = require("modernizr");
 (function(window) {
     window.customFonts.forEach(function(font){
         var observer = new FontFaceObserver(font.name, font.props);
-        var className = 'font-' + font.name + '-' + font.props.style + '-' + font.props.weight;
+        var className = 'font-' + font.name.replace(/ /g,'-') + '-' + font.props.style + '-' + font.props.weight;
         observer.check(font.testString).then(function () {
             modernizr.addTest(className, true);
         }, function () {
