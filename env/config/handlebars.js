a2aba41ba82f0ca2e08604e711b72a71402f7bac
Stephan Gerbeth <stephan.gerbeth@gmail.com>
2017-03-04 20:47:12 +0100

    configurable handlebar task

---
+++ env/config/handlebars.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var htmlprettify = require('gulp-html-prettify');
+var htmlmin = require('gulp-htmlmin');
+
+module.exports = [
+    {
+        development: false,
+        production: true,
+        module: htmlmin({
+            collapseInlineTagWhitespace: true,
+            collapseWhitespace: true,
+            keepClosingSlash: true,
+            preserveLineBreaks: false,
+            quoteCharacter: '"',
+            minifyJS: true,
+            minifyCSS: true
+        })
+    }, {
+        development: false,
+        production: true,
+        module: htmlprettify({
+            indent_inner_html: true,
+            indent_handlebars: true,
+            brace_style: 'expand',
+            indent_size: 4  ,
+            indent_char: ' ',
+            max_char: 0,
+            unformatted: ["sub", "sup", "b", "i", "u", "script", "span"]
+        })
+    }
+];
